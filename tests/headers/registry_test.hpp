2355991220cea78cac90e15705638f1a970225b7
SOO <ssj060707+github@gmail.com>
2026-07-12 21:04:46 +0800

    registry test

--- tests/headers/registry_test.hpp
+++ tests/headers/registry_test.hpp
@@ -1,5 +1,9 @@
+#ifndef BOB_REGISTRY_TEST
+#define BOB_REGISTRY_TEST
+
 #include "bob/registry.hpp"
 
+#include <string>
 
 struct Vector2
 {
@@ -12,91 +16,197 @@ struct Tag
 
 using sample_registry = bob::registry<Tag, Vector2, std::string>;
 
-void test_registry()
+class RegistryTest()
 {
-	sample_registry r{};
+	public:
+		RegistryTest()
+		{
+			this->m_AddEntityZero();
+			this->m_AddEntityOne();
+			this->m_AddEntityTwo();
 
-	const bob::entity_handle first_handle = r.get_new_handle();
-	assert(first_handle == bob::entity_handle(0));
-	r.add<std::string>(first_handle, "first test string");
+			this->m_GetStringComponents();
+			this->m_GetVectorComponents();
+			this->m_GetTagComponents();
 
+			this->m_IterateStrings();
+			this->m_IterateStringsAndVectors();
+			this->m_IterateAll();
 
-	const bob::entity_handle second_handle = r.get_new_handle();
-	assert(second_handle == bob::entity_handle(1));
-	r.add<Vector2>(second_handle, 6.0f, 7.0f);
-	r.add<std::string>(second_handle, "second test string");
+			this->m_RemoveEntityTwo();
+			this->m_RemoveEntityOne();
+			this->m_RemoveEntityZero();
+		}
 
+	private:
+		void m_AddEntityZero()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
 
-	const bob::entity_handle third_handle = r.get_new_handle();
-	assert(third_handle == bob::entity_handle(2));
-	r.add<Tag>(third_handle);
-	r.add<Vector2>(third_handle, 12.0f, 14.0f);
-	r.add<std::string>(third_handle, "third and last test string");
+			const bob::entity_handle first_handle = this->m_Registry.get_new_handle();
+			assert(first_handle == bob::entity_handle(0));
 
-	const auto& string_set = r.get_sparse_set<std::string>();
-	const auto& string_components = string_set.get_components();
-	assert(string_components.size() == 3);
+			this->m_Registry.add<std::string>(first_handle, "0");
+			
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
 
-	const auto& vector_set = r.get_sparse_set<Vector2>();
-	const auto& vector_components = vector_set.get_components();
-	assert(vector_components.size() == 2);
+		void m_AddEntityOne()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
 
-	const auto& tag_set = r.get_sparse_set<Tag>();
-	const auto& tag_components = tag_set.get_components();
-	assert(tag_components.size() == 1);
+			const bob::entity_handle second_handle = r.get_new_handle();
+			assert(second_handle == bob::entity_handle(1));
 
-	const auto& string_handles = r.get_iterator<std::string>();
-	assert(string_handles.size() == 3);
+			this->m_Registry.add<Vector2>(second_handle, 6.0f, 7.0f);
+			this->m_Registry.add<std::string>(second_handle, "1");
+			
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
 
+		void m_AddEntityTwo()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
 
-	std::cout << "STRING ONLY" << "\n";
-	for (auto i : string_handles)
-		std::cout << i.index() << ": " << string_set[i] << std::endl;
-	std::cout << "\n";
+			const bob::entity_handle third_handle = r.get_new_handle();
+			assert(third_handle == bob::entity_handle(2));
 
+			this->m_Registry.add<Tag>(third_handle);
+			this->m_Registry.add<Vector2>(third_handle, 12.0f, 14.0f);
+			this->m_Registry.add<std::string>(third_handle, "2");
+			
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
 
-	const auto& string_vector_handles = r.get_iterator<Vector2, std::string>();
-	assert(string_vector_handles.size() == 2);
+		void m_GetStringComponents()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
 
-	std::cout << "STRING AND VECTOR" << "\n";
-	for (uint32_t i = 0, n = static_cast<uint32_t>(string_vector_handles.size()); i < n; i++)
-	{
-		const bob::entity_handle handle = string_vector_handles[i];
+			const auto& string_set = this->m_Registry.get_sparse_set<std::string>();
+			const auto& string_components = string_set.get_components();
 
-		std::cout << handle.index() << ": " <<
-			string_set[handle] << ", "
-			<< vector_set[handle].x << ", "
-			<< vector_set[handle].y
-			<< std::endl;
-	}
-	std::cout << "\n";
+			assert(string_components.size() == 3);
+			
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
 
+		void m_GetVectorComponents()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
 
-	const auto& string_vector_tag_handles = r.get_iterator<Tag, Vector2, std::string>();
-	assert(string_vector_tag_handles.size() == 1);
+			const auto& vector_set = this->m_Registry.get_sparse_set<Vector2>();
+			const auto& vector_components = vector_set.get_components();
 
-	std::cout << "STRING VECTOR AND TAG\n";
-	const bob::entity_handle handle = string_vector_tag_handles[0];
-	std::cout
-		<< handle.index()
-		<< ": "
-		<< string_set[handle]
-		<< ", "
-		<< vector_set[handle].x
-		<< ", "
-		<< vector_set[handle].y
-		<< std::endl;
-	std::cout << "\n";
+			assert(vector_components.size() == 2);
+			
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+		
+		void m_GetTagComponents()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
 
-	r.remove<Tag, Vector2, std::string>(third_handle);
-	const auto& string_vector_tag_after_r_handles = r.get_iterator<Tag, Vector2, std::string>();
-	assert(string_vector_tag_after_r_handles.size() == 0);
+			const auto& tag_set = this->m_Registry.get_sparse_set<Tag>();
+			const auto& tag_components = tag_set.get_components();
 
-	r.remove<Vector2, std::string>(second_handle);
-	const auto& string_vector_after_r_handles = r.get_iterator<Vector2, std::string>();
-	assert(string_vector_after_r_handles.size() == 0);
+			assert(tag_components.size() == 1);
 
-	r.remove<std::string>(first_handle);
-	const auto& string_after_r_handles = r.get_iterator<std::string>();
-	assert(string_vector_after_r_handles.size() == 0);
-}
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+
+		void m_IterateStrings()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
+
+			const auto& string_handles = this->m_Registry.get_iterator<std::string>();
+			assert(string_handles.size() == 3);
+
+			const bob::sparse_set<std::string>& string_set = this->m_Registry.get_sparse_set<std::string>();
+
+			for (size_t i = 0, n = string_handles.size(); i < n; ++i)
+				assert(string_handles[i] == std::to_string(i));
+
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+
+		void m_IterateStringsAndVectors()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
+
+			const auto& string_vector_handles = this->m_Registry.get_iterator<Vector2, std::string>();
+			assert(string_vector_handles.size() == 2);
+
+			const bob::sparse_set<std::string>& string_set = this->m_Registry.get_sparse_set<std::string>();
+			const bob::sparse_set<Vector2>& vector_set = this->m_Registry.get_sparse_set<Vector2>();
+
+			for (size_t i = 0, n = string_vector_handles.size(); i < n; ++i)
+			{
+				const bob::entity_handle handle = string_vector_handles[i];
+
+				assert(string_set[handle] == std::to_string(i));
+				assert(vector_set[handle].x == static_cast<float>((i + 1) * 6.0f));
+				assert(vector_set[handle].y == static_cast<float>((i + 1) * 7.0f));
+			}
+
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+
+		void m_IterateAll()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
+
+			const auto& string_vector_tag_handles = this->m_Registry.get_iterator<Tag, Vector2, std::string>();
+			assert(string_vector_tag_handles.size() == 1);
+
+			const bob::sparse_set<std::string>& string_set = this->m_Registry.get_sparse_set<std::string>();
+			const bob::sparse_set<Vector2>& vector_set = this->m_Registry.get_sparse_set<Vector2>();
+
+			const bob::entity_handle handle = string_vector_tag_handles[0];
+
+			assert(string_set[handle] == "2");
+			assert(vector_set[handle].x == 12.0f);
+			assert(vector_set[handle].y == 14.0f);
+
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+
+		void m_RemoveEntityTwo()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
+
+			this->m_Registry.remove<Tag, Vector2, std::string>(third_handle);
+
+			const auto& string_vector_tag_handles = this->m_Registry.get_iterator<Tag, Vector2, std::string>();
+			assert(string_vector_tag_handles.size() == 0);
+
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+
+		void m_RemoveEntityOne()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
+
+			this->m_Registry.remove<Vector2, std::string>(second_handle);
+
+			const auto& string_vector_handles = this->m_Registry.get_iterator<Vector2, std::string>();
+			assert(string_vector_handles.size() == 0);
+
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+
+		void m_RemoveEntityZero()
+		{
+			std::cout << __FILE_NAME__ << ": Running " << __FUNCTION__ << "\n";
+
+			this->m_Registry.remove<std::string>(first_handle);
+
+			const auto& string_handles = this->m_Registry.get_iterator<std::string>();
+			assert(string_handles.size() == 0);
+
+			std::cout << __FILE_NAME__ << ": " << __FUNCTION__ << " passed\n";
+		}
+
+		sample_registry m_Registry;
+};
+
+#endif
